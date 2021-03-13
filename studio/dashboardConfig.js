f37ace0b3882e04d2ef9b27477a52199e87d15cb
Richard Westmoreland <kegtrack@gmail.com>
2021-03-13 14:25:45 -0600

    Update dash config

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -13,14 +13,14 @@ export default {
         sites: [
           {
             title: 'Sanity Studio',
-            apiId: 'xxxxx-yyyy-zzzz-xxxx-yyyyyyyy',
-            buildHookId: 'xxxyyyxxxyyyyxxxyyy',
+            apiId: '5a68c5c8-cca6-4bed-b677-c1a651cee45a',
+            buildHookId: '604d1eaec49ff76c430e3e01',
             name: 'sanity-gatsby-blog-20-studio',
           },
           {
-            title: 'Website',
-            apiId: 'yyyyy-xxxxx-zzzz-xxxx-yyyyyyyy',
-            buildHookId: 'yyyyxxxxxyyyxxdxxx',
+            title: 'Frontend',
+            apiId: '8e2849a6-4bfb-4530-addc-3022eafe39ee',
+            buildHookId: '604d1f5353c698b84cefed6a',
             name: 'sanity-gatsby-blog-20-web',
           },
         ],
