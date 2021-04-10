1fcf1f20ae5ded002a3d7e9f254659e5641d27b8
Richard Westmoreland <kegtrack@gmail.com>
2021-04-09 23:41:05 -0400

    schema

--- studio/schemas/documents/contact.js
+++ studio/schemas/documents/contact.js
@@ -15,31 +15,24 @@ export default {
       title: 'Created at',
     },
     {
-      name: 'data',
-      type: 'object',
-      title: 'Data',
-      fields: [
-        {
-          name: 'email',
-          type: 'email',
-          title: 'Email',
-        },
-        {
-          name: 'firstname',
-          type: 'string',
-          title: 'First Name',
-        },
-        {
-          name: 'lastname',
-          type: 'string',
-          title: 'Last Name',
-        },
-        {
-          name: 'message',
-          type: 'text',
-          title: 'Message',
-        },
-      ],
+      name: 'email',
+      type: 'email',
+      title: 'Email',
+    },
+    {
+      name: 'firstname',
+      type: 'string',
+      title: 'First Name',
+    },
+    {
+      name: 'lastname',
+      type: 'string',
+      title: 'Last Name',
+    },
+    {
+      name: 'message',
+      type: 'text',
+      title: 'Message',
     },
   ],
 }
